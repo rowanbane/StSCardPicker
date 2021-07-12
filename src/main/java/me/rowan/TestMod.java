7b7b32f72f242cf6a69f728f32ca912b39b088a3
rowanbane <rowanspenc@gmail.com>
2021-07-12 20:12:10 +0100

    Added card names and basic printing

--- src/main/java/me/rowan/TestMod.java
+++ src/main/java/me/rowan/TestMod.java
@@ -6,9 +6,20 @@ import basemod.interfaces.PostDungeonInitializeSubscriber;
 import basemod.interfaces.PostExhaustSubscriber;
 
 
-import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evacipated.cardcrawl.modthespire.lib.*;
+import com.evacipated.cardcrawl.modthespire.patcher.PatchingException;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
+import com.megacrit.cardcrawl.screens.CardRewardScreen;
+import javassist.CannotCompileException;
+import javassist.CtBehavior;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 @SpireInitializer
 public class TestMod implements PostExhaustSubscriber,
@@ -47,7 +58,6 @@ public class TestMod implements PostExhaustSubscriber,
     public void receivePostDungeonInitialize() {
 
 
-
         resetCounts();
     }
 
@@ -58,4 +68,33 @@ public class TestMod implements PostExhaustSubscriber,
         System.out.println("Counts incremented");
 
     }
+
+    @SpirePatch(clz = CardRewardScreen.class, method = "renderCardReward")
+    public static class RenderPrediction {
+
+
+        @SpireInsertPatch(locator = Locator.class)
+        public static void patch(CardRewardScreen __instance, SpriteBatch sb) {
+
+            for (AbstractCard c : __instance.rewardGroup) {
+                FontHelper.renderSmartText(sb,
+                        FontHelper.topPanelAmountFont,
+                        c.name,
+                        c.hb.x,
+                        c.hb.y,
+                        Color.WHITE);
+            }
+        }
+    }
+
+
+    //Locator class to aid in finding the render point - From Slay AI
+    private static class Locator extends SpireInsertLocator {
+        public int[] Locate(CtBehavior ctMethodToPatch) throws CannotCompileException, PatchingException {
+            Matcher finalMatcher = new Matcher.MethodCallMatcher(AbstractCard.class, "render");
+            return LineFinder.findInOrder(ctMethodToPatch, new ArrayList<>(), finalMatcher);
+        }
+    }
+
 }
+
